a6d531ec9b1484a0b91769e87f81e7aa85cb8ef3
Dheeraj Chittari <dheerajvarma1022@gmail.com>
2023-05-14 13:48:13 +0200

    added aws deploy script to travis

--- readme.sh
+++ readme.sh
@@ -17,3 +17,23 @@ docker build -t complex-client:latest -f ./client/Dockerfile.dev ./client
 # Commit and push the changes to the repository
 # Check the build status in TravisCi
 
+# To deploy the application to AWS
+# The first thing you need to do is to create an EC2 IAM role. (lec 88)
+# After you have to create an EBS environment with above role.
+# you may need to make a modification to the S3 bucket. (not required anymore)
+# Create an IAM user with administrator access to Elastic Beanstalk (lec 88).
+
+# So far: EC2 has access role to calling AWS services (such as EBS) on our belahf. IAM user has administrotor access to EBS.
+
+# for multi container
+# create a RDS and elasticcache redis instances.
+# create and apply the security group (in VPC) for redis and RDS and EBS. (Securty group contraols how traffic must flow into our AWS instance by specifying ports 5432-6379 and protocols)
+
+
+
+
+# Create an Elastic Beanstalk application
+# Add the access key and secret key as environment variables in TravisCi
+# Add the region and app name as environment variables in TravisCi
+
+
